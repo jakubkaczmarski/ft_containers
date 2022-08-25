bff27c8276c6034af0240cbbda5f0a9127aa709d
= <kuba.kaczmarski312@gmail.com>
2022-08-25 13:23:06 +0200

    class

--- Vector.hpp
+++ Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 13:16:23 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/25 13:17:50 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/08/25 13:22:51 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,13 @@ template<typename T>
 class vector
 {
     public:
-    
+    void vector(){};
 
     private:
-
+    T *arr;
+    T *beg;
+    T *end;
+    T  *capacity_ptr;
 };
 
 #endif
