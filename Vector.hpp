9a565c847bdea46d48b4ed4bf525b43e2a4cf095
= <kuba.kaczmarski312@gmail.com>
2022-09-06 20:43:39 +0200

    structure

--- Vector.hpp
+++ Vector.hpp
@@ -6,35 +6,38 @@
 /*   By: jkaczmar <jkaczmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/25 13:16:23 by jkaczmar          #+#    #+#             */
-/*   Updated: 2022/08/25 20:44:58 by jkaczmar         ###   ########.fr       */
+/*   Updated: 2022/09/06 20:43:28 by jkaczmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef VECTOR_HPP
 #define VECTOR_HPP
 
+#include <iostream>
+
+
+//Allocate twice as much as you need with each reallocation
+//What do we need in Vector :(((())))
+//vec_ay allocated on the heap :))
+//Pointer to the begining :()
+//Pointer to the end :()
+//Pointer to allocated memory :)
 template<typename T>
-class vector
+class Vector
 {
     public:
-    vector<T>(){
-        arr = new T[1];
-        current = 0;
-        capacity = 1;
-    }
-    vector<T>(T val){
-        capacity = val * 2;
-        arr = new T[capacity];
-        current = val;
-    };
-    ~vector<T>()
+    Vector<T>();
+    Vector<T>(size_t num_of_el){};
+    void    push_back(const T &el)
     {
-        delete [] arr;
-    }
+    };
+    T   pop_back();
+    size_t  capacity();
+    size_t  size();
     private:
-    T *arr;
-    int current;
-    int  capacity;
-};
-
+    T   *array_;
+    T   *beg_;
+    T   *end_;
+    T   *cap_;
+}
 #endif
