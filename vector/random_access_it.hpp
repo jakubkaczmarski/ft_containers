14efa5e3aabc65d0ff0d8b667234c035d0969d97
= <kuba.kaczmarski312@gmail.com>
2022-10-30 19:06:12 +0100

    Constant_random_access iterator

--- vector/random_access_it.hpp
+++ vector/random_access_it.hpp
@@ -13,7 +13,7 @@ struct biderectional_iterator_tag : forward_iterator_tag {};
 struct random_access_iterator_tag : biderectional_iterator_tag {};
 
 //Class T tags
-
+template<typename T>
 class constant_random_access_iterator;
 
 template <typename Category, typename T, typename Distance = ptrdiff_t, typename Pointer = T*, typename Reference = T&>
@@ -148,11 +148,132 @@ class random_access_iterator : public iterator<random_access_iterator_tag, T>
     private:
         pointer ptr_;
 };
-
-class constant_random_access_iterator
+template<typename T>
+class constant_random_access_iterator : public iterator<random_access_iterator_tag, T>
 {
-    
-}
+    public:
+        typedef typename ft::iterator<random_access_iterator_tag, T>::value_type value_type;
+        typedef typename ft::iterator<random_access_iterator_tag, T>::difference_type difference_type;
+        typedef typename ft::iterator<random_access_iterator_tag, T>::pointer pointer;
+        typedef typename ft::iterator<random_access_iterator_tag, T>::reference reference;
+        typedef typename ft::iterator<random_access_iterator_tag, T>::iterator_category iterator_category;
+
+        constant_random_access_iterator() : ptr_(0){};
+        template <typename P>
+        constant_random_access_iterator(const random_access_iterator<P>  &rhs) : ptr_(rhs.base()){}
+        constant_random_access_iterator(const constant_random_access_iterator  &rhs) : ptr_(rhs.base()){}
+        constant_random_access_iterator(pointer p) : ptr_(p){};
+        pointer base() const
+        {
+            return (ptr_);
+        }
+        reference operator *()
+        {
+            return *(ptr_);
+        }
+        reference operator =(constant_random_access_iterator<T> const &rhs)
+        {
+            ptr_ = rhs.base();
+            return *(this);
+        }
+        reference operator ++()
+        {
+            ptr_++;
+            return *(this);
+        }
+        reference operator ++(int)
+        {
+            constant_random_access_iterator tmp(ptr_);
+            ptr_++;
+            return tmp;
+        }
+        reference operator --()
+        {
+            ptr_--;
+            return *(this);
+        }
+        reference operator --(int)
+        {
+            constant_random_access_iterator tmp (ptr_);
+            ptr_++;
+            return (tmp);
+        }
+        reference operator +(difference_type num) const
+        {
+            return (ptr_ + num);
+        }
+        reference operator -(difference_type num) const
+        {
+            return (ptr_ - num);
+        }
+        bool operator <(constant_random_access_iterator<T> rhs) const
+        {
+            return (ptr_ < rhs.base());
+        }
+        // bool operator <(constant_random_access_iterator<const T> rhs) const
+        // {
+        //     return (ptr_ < rhs.base());
+        // }
+        bool operator >(constant_random_access_iterator<const T> rhs) const
+        {
+            return (ptr_ > rhs.base());
+        }
+        // bool operator >(constant_random_access_iterator<const T> rhs) const
+        // {
+        //     return (ptr_ > rhs.base());
+        // }
+        bool operator <=(constant_random_access_iterator<T> rhs)
+        {
+            return (ptr_ <= rhs.base());
+        }
+        // bool operator <=(constant_random_access_iterator<const T> rhs)
+        // {
+        //     return (ptr_ <= rhs.base());
+        // }
+        bool operator >=(constant_random_access_iterator<T> rhs)
+        {
+            return (ptr_ >= rhs.base());
+        }
+        // bool operator >=(constant_random_access_iterator<const T> rhs)
+        // {
+        //     return (ptr_ >= rhs.base());
+        // }
+        bool operator ==(constant_random_access_iterator<T> rhs)
+        {
+            return (ptr_ == rhs.base());
+        }
+        // bool operator ==(constant_random_access_iterator<const T> rhs)
+        // {
+        //     return (ptr_ == rhs.base());
+        // }
+        bool operator !=(constant_random_access_iterator<T> rhs)
+        {
+            return (ptr_ != rhs.base());
+        }
+        // bool operator !=(constant_random_access_iterator<const T> rhs)
+        // {
+        //     return (ptr_ != rhs.base());
+        // }
+        reference operator +=(difference_type num) const
+        {
+            return (ptr_ += num);
+        }
+        reference operator -=(difference_type num) const
+        {
+            return (ptr_ -= num);
+        }
+        pointer operator->()const
+        {
+            return (ptr_);
+        }
+        reference operator[](difference_type n) const
+        {
+            return *(ptr_ + n);
+        }
+    private:
+        pointer ptr_;
+};
+
 }
 
 
