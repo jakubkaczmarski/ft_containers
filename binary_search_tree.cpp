f6f620d6ddf1404cfdb862a043562631793ce113
jakubkaczmarski <kuba.kaczmarski312@gmail.com>
2022-11-04 01:33:28 +0100

    tree

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -40,6 +40,33 @@ struct Node *insert(struct Node *node, int key)
     return node;
 }
 
+
+struct Node *deleteNode(struct Node *root, int key)
+{
+    if(root == NULL)
+        return root;
+    
+    if(key < root->key)
+    {
+        root->left = deleteNode(root->left, key);
+    }else if(key > root->key){
+        root->right = deleteNode(root->right, key);
+    }else
+    {
+        if(root->left == NULL)
+        {
+            struct Node *tmp = root->right;
+            free(root);
+            return tmp;
+        }else if(root->right == NULL){
+            struct Node  *tmp = root->left;
+            free(root);
+            return tmp;
+        }
+    }
+    return  NULL;
+}
+
 int main()
 {
     struct Node *root = NULL;
@@ -48,6 +75,7 @@ int main()
     root = insert(root, 3);
     root = insert(root, -32);
     root = insert(root, 1);
+    deleteNode(root, 5);
     std::cout << "Inorder traversal" << std::endl;
     inorder(root);
 }
