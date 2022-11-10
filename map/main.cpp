6cd034a70aac9ea0914f6167d91924e8055da696
= <kuba.kaczmarski312@gmail.com>
2022-11-10 21:36:00 +0100

    cleanup

--- map/main.cpp
+++ map/main.cpp
@@ -1,9 +1,32 @@
-#include "../additional.hpp"
-
-
+#include "red_black_tree.hpp"
+#include <map>
+#include <iostream>
+#include <iterator>
+#include "RedBlackTreeIterator.hpp"
 int main()
 {
-    int b = 2;
-    double c = 3.1;
-    pair<int, double> s  = make_pair(b, c);
+
+    //Red black tree iterator
+    //
+    std::map<int, std::string> mapp;
+
+    mapp.insert(std::make_pair<int, std::string>(2, "Siemanko"));
+    std::map<int, std::string>::iterator iter_map = mapp.begin();
+    std::cout << (*iter_map).first << std::endl;
+    // int b;
+    RBT<int, std::string> s;
+    s.insert(1);
+    s.insert(0);
+    s.insert(6);
+    s.insert(2);
+    // s.
+    std::cout << "Red black trees are cool"  << std::endl;
+    s.print_tree();
+    int b = 1;
+    struct Node<int, std::string> * ptr = s.search(1);
+    std::cout << ptr->color << std::endl;
+    std::cout << ptr->key << std::endl;
+
+    s.delete_element(6);
+    s.print_tree();
 }
